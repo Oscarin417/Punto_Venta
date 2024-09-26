3b04a14573c958b8e78423eee05fa780b18e15ca
oscar <oscar@gmail.com>
2024-09-25 19:11:14 -0600

    sqlite3

--- punto_venta/settings.py
+++ punto_venta/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'punto_venta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db',
     }
 }
 
